81beb9f63b6f96531b940cccced6775b580e06b8
tomokiono <tom_ppg@yahoo.co.jp>
2020-07-19 23:03:25 +0900

    ユーザー登録、編集機能と投稿機能を実装

--- app/controllers/public/endusers_controller.rb
+++ app/controllers/public/endusers_controller.rb
@@ -6,5 +6,16 @@ class Public::EndusersController < ApplicationController
   end
 
   def update
+    @enduser = current_enduser
+    if @enduser.update(enduser_params)
+      redirect_to public_endusers_path(@customer), notice: "登録情報の編集に成功しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def enduser_params
+    params.require(:enduser).permit(:name, :email, :introduction, :area, :age, :image)
   end
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,17 +1,27 @@
 class Public::PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
   end
-  
+
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.enduser_id = current_enduser.id
+    if @post.save
+      redirect_to public_posts_path, notice: "募集の投稿に成功しました！"
+    else
+      render :new
+    end
   end
 
   def update
@@ -19,4 +29,10 @@ class Public::PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :is_valid)
+  end
+
 end
