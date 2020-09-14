4179f5c2a8563a4955742900103c4c894e60b4b7
tomoki ono <tom_ppg@yahoo.co.jp>
2020-09-14 19:18:01 +0000

    チャート機能を修正

--- app/controllers/admins/charts_controller.rb
+++ app/controllers/admins/charts_controller.rb
@@ -3,23 +3,37 @@ class Admins::ChartsController < ApplicationController
     # 期間別の会員者数、投稿数の集計データー
     # gem"chartKick"を使用する場合、二重配列のデータをviewに渡してあげると、グラフの形で表示できる。eD,pD,eM
     # eD,pDはenduserとpostをcreated_atカラムで日ごと集計したもの. [["2020-08-17", 0], ["2020-08-18", 0], ...]　みたいな形になってる
-    eD = Enduser.all.group_by_day(:created_at, format: "%b/%d").count.to_a
-    pD = Post.all.group_by_day(:created_at, format: "%b/%d").count.to_a
+    eD = Enduser.all.group_by_day(:created_at, format: "%b/%d").count.to_h
+    pD = Post.all.group_by_day(:created_at, format: "%b/%d").count.to_h
     # eM,pMはenduserとpostをcreated_atカラムで月ごと集計したもの. [["2020-08-01", 0], ["2020-08-01", 0], ...]　みたいな形になってる
-    eM = Enduser.all.group_by_month(:created_at, format: "%Y/%b").count.to_a
-    pM = Post.all.group_by_month(:created_at, format: "%Y/%b").count.to_a
+    eM = Enduser.all.group_by_month(:created_at, format: "%Y/%b").count.to_h
+    pM = Post.all.group_by_month(:created_at, format: "%Y/%b").count.to_h
 
     # 一週間の会員登録者数、投稿数
-    eD.count >= 7 ? @endusersWeek = eD.slice(-7..-1) : @endusersWeek = nil
-    pD.count >= 7 ? @postsWeek = pD.slice(-7..-1) : @postsWeek = nil
+    @endusersWeek = []
+    @postsWeek = []
+    (1..7).each do |i|
+      @endusersWeek << [i.days.ago.strftime('%b/%d'), eD.fetch("#{i.days.ago.strftime('%b/%d')}", 0)]
+      @postsWeek << [i.days.ago.strftime('%b/%d'), pD.fetch("#{i.days.ago.strftime('%b/%d')}", 0)]
+    end
 
     # 一ヶ月の会員登録者数、投稿数
-    eD.count >= 30 ? @endusersMonth = eD.slice(-30..-1) : @endusersMonth = nil
-    pD.count >= 30 ? @postsMonth = pD.slice(-30..-1) : @postsMonth = nil
+    @endusersMonth = []
+    @postsMonth = []
+    (1..30).each do |i|
+      @endusersMonth << [i.days.ago.strftime('%b/%d'), eD.fetch("#{i.days.ago.strftime('%b/%d')}", 0)]
+      @postsMonth << [i.days.ago.strftime('%b/%d'), pD.fetch("#{i.days.ago.strftime('%b/%d')}", 0)]
+    end
 
     # 一年の会員登録者数、投稿数
-    eM.count >= 12 ? @endusersYear = eM.slice(-12..-1) : @endusersYear = nil
-    pM.count >= 12 ? @postsYear = pM.slice(-12..-1) : @postsYear = nil
+    @endusersYear = [[Date.today.beginning_of_month.strftime('%Y/%b'), eM.fetch("#{Date.today.beginning_of_month.strftime('%Y/%b')}", 0)]]
+    @postsYear = [[Date.today.beginning_of_month.strftime('%Y/%b'), pM.fetch("#{Date.today.beginning_of_month.strftime('%Y/%b')}", 0)]]
+    (1..11).each do |i|
+      @endusersYear << [i.months.ago.beginning_of_month.strftime('%Y/%b'), eM.fetch("#{i.months.ago.beginning_of_month.strftime('%Y/%b')}", 0)]
+      @postsYear << [i.months.ago.beginning_of_month.strftime('%Y/%b'), pM.fetch("#{i.months.ago.beginning_of_month.strftime('%Y/%b')}", 0)]
+    end
+    # @endusersYear << [Date.today.beginning_of_month.strftime('%Y/%b'), eM.fetch("#{Date.today.beginning_of_month.strftime('%Y/%b')}", 0)]
+    # @postsYear << [Date.today.beginning_of_month.strftime('%Y/%b'), pM.fetch("#{Date.today.beginning_of_month.strftime('%Y/%b')}", 0)]
     # 期間別の会員者数、投稿数の集計データーここまで
 
 
